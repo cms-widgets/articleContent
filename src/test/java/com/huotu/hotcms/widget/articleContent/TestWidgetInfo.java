4cc177c82870a231b2a928bfadd6fbe84c287dd0
lihuaixin <1072931117@qq.com>
2016-12-05 10:02:34 +0800

    测试修改

--- src/test/java/com/huotu/hotcms/widget/articleContent/TestWidgetInfo.java
+++ src/test/java/com/huotu/hotcms/widget/articleContent/TestWidgetInfo.java
@@ -9,14 +9,14 @@
 
 package com.huotu.hotcms.widget.articleContent;
 
-import java.io.IOException;
-import java.util.function.Function;
-
 import com.huotu.hotcms.widget.ComponentProperties;
 import com.huotu.hotcms.widget.Widget;
 import com.huotu.hotcms.widget.WidgetStyle;
+import com.huotu.widget.test.Editor;
 import com.huotu.widget.test.WidgetTest;
 import org.openqa.selenium.WebElement;
+
+import java.io.IOException;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -31,10 +31,10 @@ public class TestWidgetInfo extends WidgetTest {
     }
 
     @Override
-    protected void editorWork(Widget widget, WebElement editor, Supplier<Map<String, Object>> currentWidgetProperties) {
-        Map map = currentWidgetProperties.get();
-        //todo
+    protected void editorWork(Widget widget, Editor editor, Supplier<Map<String, Object>> currentWidgetProperties) throws IOException {
+
     }
+
 
     @Override
     protected void browseWork(Widget widget, WidgetStyle style, Function<ComponentProperties, WebElement> uiChanger)
